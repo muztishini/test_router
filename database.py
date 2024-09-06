0c4ab596801205d0db408002d9f5a169b33155e0
muztishini <111636144+muztishini@users.noreply.github.com>
2024-09-06 12:04:35 +0500

    first commit

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+
+
+Base.metadata.create_all(bind=engine)
---
+++ item_router.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Body
+from database import Item, SessionLocal
+from fastapi.responses import JSONResponse
+
+
+item_router = APIRouter()
+
+@item_router.get("/")
+async def read_items():
+    db = SessionLocal()
+    items = db.query(Item).all()
+    return items
+
+
+@item_router.post("/")
+async def create_item(data = Body()):
+    db = SessionLocal()
+    db_item = Item(name=data["name"], description=data["description"])
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+@item_router.get("/{item_id}")
+async def read_item(item_id: int):
+    db = SessionLocal()
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item==None:  
+        return JSONResponse(status_code=404, content={ "message": f"Пользователь {item_id} не найден"})
+    return item
+
+
+@item_router.put("/{item_id}")
+async def update_item(item_id: int, data = Body()):
+    db = SessionLocal()
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if db_item==None:  
+        return JSONResponse(status_code=404, content={ "message": f"Пользователь {item_id} не найден"})
+    db_item.name = data["name"]
+    db_item.description = data["description"]
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+@item_router.delete("/{item_id}")
+async def delete_item(item_id: int):
+    db = SessionLocal()
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if db_item==None:  
+        return JSONResponse(status_code=404, content={ "message": f"Пользователь {item_id} не найден"})
+    db.delete(db_item)
+    db.commit()
+    return {"message": f"Item {item_id} deleted successfully"}
